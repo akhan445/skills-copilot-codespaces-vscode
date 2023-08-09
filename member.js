9b491d7e7e3e2747f7680e9095ee14a00a5a06ad
Abdullah Khan <abdullahshamskhan@gmail.com>
2023-08-09 22:27:16 +0000

    Copilt second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var memberValue = member.value;
+    var skillValue = skill.value;
+    var memberArray = memberValue.split(",");
+    var skillArray = skillValue.split(",");
+    var memberArrayLength = memberArray.length;
+    var skillArrayLength = skillArray.length;
+    var memberSkill = document.getElementById("memberSkill");
+    var memberSkillValue = "";
+    var memberSkillArray = [];
+    var memberSkillArrayLength = 0; 
+}
